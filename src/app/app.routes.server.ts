58453b0b3cddaf04ee33bc61e699befb9c210022
MaryamElmasry <maryam.mohamed.elmasry112@gmail.com>
2025-03-09 21:04:08 +0400

    updated  app routes server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,29 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'checkout/:id', //dashboard
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'brands/:id', //dashboard
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'categoryDetails/:id', //dashboard
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'details/:id', //dashboard
+    renderMode: RenderMode.Client,
+  },
+
+
+  // {
+  //   path: '**', //news website
+  //   renderMode: RenderMode.Server
+  // },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
   }
 ];
